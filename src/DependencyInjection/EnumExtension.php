c450eaeea0340779c06e999744c8faec139c3163
Yann Eugoné <yeugone@prestaconcept.net>
2020-06-25 23:24:50 +0200

    Removed configuration

--- src/DependencyInjection/EnumExtension.php
+++ src/DependencyInjection/EnumExtension.php
@@ -12,7 +12,6 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Yokai\EnumBundle\EnumInterface;
-use Yokai\EnumBundle\EnumRegistry;
 
 /**
  * @author Yann Eugoné <eugone.yann@gmail.com>
@@ -24,9 +23,6 @@ class EnumExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $xmlLoader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $xmlLoader->load('enum.xml');
 
@@ -47,13 +43,7 @@ class EnumExtension extends Extension
             $xmlLoader->load('twig.xml');
         }
 
-        if ($config['enum_autoconfiguration']) {
-            $container->registerForAutoconfiguration(EnumInterface::class)
-                ->addTag('enum');
-        }
-
-        if ($config['enum_registry_autoconfigurable']) {
-            $container->setAlias(EnumRegistry::class, 'enum.registry');
-        }
+        $container->registerForAutoconfiguration(EnumInterface::class)
+            ->addTag('enum');
     }
 }
--- tests/DependencyInjection/EnumExtensionTest.php
+++ tests/DependencyInjection/EnumExtensionTest.php
@@ -21,65 +21,25 @@ class EnumExtensionTest extends TestCase
     /**
      * @test
      */
-    public function it_register_enum_for_autoconfiguration_by_default(): void
-    {
-        $container = new ContainerBuilder();
-        $this->extension()->load([[]], $container);
-
-        $autoconfigure = $container->getAutoconfiguredInstanceof();
-        $this->assertArrayHasKey(EnumInterface::class, $autoconfigure);
-        $this->assertEquals(['enum' => [[]]], $autoconfigure[EnumInterface::class]->getTags());
-    }
-
-    /**
-     * @test
-     */
-    public function it_do_not_register_enum_for_autoconfiguration_if_asked_to(): void
-    {
-        $container = new ContainerBuilder();
-        $this->extension()->load([['enum_autoconfiguration' => false]], $container);
-
-        $autoconfigure = $container->getAutoconfiguredInstanceof();
-        $this->assertArrayNotHasKey(EnumInterface::class, $autoconfigure);
-    }
-
-    /**
-     * @test
-     */
-    public function it_register_enum_registry_alias_for_autowire_by_default(): void
-    {
-        $container = new ContainerBuilder();
-        $this->extension()->load([[]], $container);
-        $this->assertTrue($container->hasAlias(EnumRegistry::class));
-    }
-
-    /**
-     * @test
-     */
-    public function it_do_not_register_enum_registry_alias_for_autowire_if_asked_to(): void
-    {
-        $container = new ContainerBuilder();
-        $this->extension()->load([['enum_registry_autoconfigurable' => false]], $container);
-        $this->assertFalse($container->hasAlias(EnumRegistry::class));
-    }
-
-
-    /**
-     * @test
-     */
     public function it_register_services(): void
     {
         $container = new ContainerBuilder();
         $this->extension()->load([[]], $container);
+
         $services = [
             'yokai_enum.form_type.enum_type',
             'yokai_enum.form_extension.enum_type_guesser',
             'yokai_enum.validator_constraints.enum_validator',
             'yokai_enum.twig_extension.enum_extension',
         ];
-
         foreach ($services as $service) {
             self::assertTrue($container->has($service), sprintf('Service "%s" is registered', $service));
         }
+
+        $autoconfigure = $container->getAutoconfiguredInstanceof();
+        $this->assertArrayHasKey(EnumInterface::class, $autoconfigure);
+        $this->assertEquals(['enum' => [[]]], $autoconfigure[EnumInterface::class]->getTags());
+
+        $this->assertTrue($container->hasAlias(EnumRegistry::class));
     }
 }
