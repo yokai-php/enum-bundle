27d2fece5bd50c31daa8d2dd8c35942a80f560b3
Yann Eugoné <yeugone@prestaconcept.net>
2021-05-24 16:57:42 +0200

    Configure PHPCS to require strict types

--- src/Enum.php
+++ src/Enum.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yokai\EnumBundle;
 
 use LogicException;
--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Yokai\EnumBundle\Exception;
 
 use Throwable;
